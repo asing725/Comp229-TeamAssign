a8aa2ee57a0601542699bcfc6ebf02b5a09833d3
Iamatcentennial <ashish.raist@gmail.com>
2017-12-20 22:00:38 -0500

    Add project files.

---
+++ Comp229-TeamAssign/AddNewBook.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comp229_TeamAssign
+{
+    public partial class AddNewBook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addNewBook_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cancelAddNewBoook_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
